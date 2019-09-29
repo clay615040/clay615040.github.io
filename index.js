94951a37141260f514073244eba96ee023a99921
jeffery615040 <jeffery615040@gmail.com>
2019-09-29 16:32:39 +0800

    test

---
+++ index.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper('.story', {
+    scrollbar: {
+        loop: true,
+        speed: 2000,
+        el: '.swiper-scrollbar',
+        hide: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+var swiper = new Swiper('.system_at', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper('.system_fighting', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper('.system_machine', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper('.character1', {
+    pagination: {
+        loop: true,
+        speed: 4000,
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+var swiper = new Swiper('.character2', {
+    pagination: {
+        loop: true,
+        speed: 4000,
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
