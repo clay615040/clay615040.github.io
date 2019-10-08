c57531219ea330e4ee27005b5e2411d73dffd430
jeffery615040 <jeffery615040@gmail.com>
2019-10-08 18:15:25 +0800

    Update vue1.js

--- vue1.js
+++ vue1.js
@@ -7,8 +7,9 @@ var app = new Vue({
     methods: {
         addTodo: function (todo) {
             this.todos.push({content:todo , completed:false});
-            $("#aa2").val("123");
+            
         },
+        
         removeTodo: function (todo) {
             this.todos.splice(this.todos.indexOf(todo),1);
         }
