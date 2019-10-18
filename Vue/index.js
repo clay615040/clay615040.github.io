4d464d7dfa06fa58a967a888c79ac7745a96df9c
jeffery615040 <jeffery615040@gmail.com>
2019-10-18 15:42:12 +0800

    add vue1

---
+++ Vue/index.js
@@ -0,0 +1,27 @@
+let data = {
+    input: {
+        type: 'Alex教學',
+        title: ''
+    },
+    menu: [
+        {type:'Alex教學',title:'表單雙向資料',link:'./vue1.html'}
+    ]
+}
+
+let vm = new Vue({
+    el: '#app',
+    data: data,
+    methods: {
+        inputHandler() {
+            if (this.input.title) {
+                let { type, title } = this.input
+                this.menu.push({
+                    type,
+                    title,
+                    link: 'javascript:;'
+                })
+                this.input.title = ''
+            }
+        }
+    }
+})
---
+++ Vue/vue1.js
@@ -0,0 +1,14 @@
+let data = {
+    name:'Jeffery',
+    sex:'男',
+    age:'21~30歲',
+    favor:['打扣','打電玩','打東東'],
+    meat:true
+}
+
+
+let vm = new Vue({
+    el: '#app',
+    data: data
+})
+
