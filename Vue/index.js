9df2eeb303188b048ac16548b2c7c84093713e07
jeffery615040 <jeffery615040@gmail.com>
2019-10-21 13:50:33 +0800

    新增 天氣卡片 JQ分頁

--- Vue/index.js
+++ Vue/index.js
@@ -1,28 +1,39 @@
 let data = {
     input: {
-        type: 'Alex教學',
+        type: '全部',
         title: ''
     },
     menu: [
-        {type:'Alex教學',title:'表單雙向資料',link:'./vue1.html'},
-        {type:'六角學院',title:'代辦表單',link:'./sixvue1.html'}
+        { type: 'Alex教學', title: '表單雙向資料', link: './vue1.html' },
+        { type: '六角學院', title: '代辦表單', link: './sixvue1.html' },
+        { type: '練習', title: 'API天氣卡片', link: '/js+jq/weather_card.html' },
+        { type: '練習', title: 'JQ分頁', link: '/js+jq/ulli+jq.html' }
     ]
 }
 
 let vm = new Vue({
     el: '#app',
     data: data,
-    methods: {
-        inputHandler() {
-            if (this.input.title) {
-                let { type, title } = this.input
-                this.menu.push({
-                    type,
-                    title,
-                    link: 'javascript:;'
-                })
-                this.input.title = ''
-            }
-        }
-    }
+    computed: {
+        typeMenu() {
+            if (this.input.type != '全部'){
+                return this.menu.filter( item => {
+                    return item.type == this.input.type
+            })
+            }else{
+                return this.menu
+            }           
+        },
+        titleMenu(){
+            if(this.input.title){
+                return this.typeMenu.filter( item => {
+                    let content = item.title.toLowerCase()
+                    let keyword = this.input.title.toLowerCase()
+                    return content.indexOf(keyword) != -1
+                })}
+                else{
+                return this.typeMenu
+                }
+        }    
+}
 })
